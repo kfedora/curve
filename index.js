5879ae1ba3d006d67e59ee6681cac4e332d2bc0e
k <kfedora@protonmail.com>
2020-10-07 10:58:51 -0600

    Update index.js
    https://stackoverflow.com/questions/48864589/how-to-scrape-multi-level-links-using-puppeteer-js
    
    
    
    If you try opening a new tab for each ICO page in parallel you might end up with 100+ pages loading at the same time.
    
    So the best thing you could do is to first collect the URLs and then visit them one by one in a loop.
    
    This also allows keeping the code simple and readable.
    
    For example (please, see my comments):
    
    const browser = await puppeteer.launch({ headless: false });
    
    const page = await browser.newPage();
    
    await page.goto('https://tokenmarket.net/blockchain/');
    
    // Gather assets page urls for all the blockchains
    const assetUrls = await page.$$eval(
      '.table-assets > tbody > tr .col-actions a:first-child',
      assetLinks => assetLinks.map(link => link.href)
    );
    
    const results = [];
    
    // Visit each assets page one by one
    for (let assetsUrl of assetUrls) {
      await page.goto(assetsUrl);
    
      // Now collect all the ICO urls.
      const icoUrls = await page.$$eval(
        '#page-wrapper > main > div.container > div > table > tbody > tr > td:nth-child(2) a',
        links => links.map(link => link.href)
      );
    
      // Visit each ICO one by one and collect the data.
      for (let icoUrl of icoUrls) {
        await page.goto(icoUrl);
    
        const icoImgUrl = await page.$eval('#asset-logo-wrapper img', img => img.src);
        const icoName = await page.$eval('h1', h1 => h1.innerText.trim());
        // TODO: Gather all the needed info like description etc here.
    
        results.push([{
          icoName,
          icoUrl,
          icoImgUrl
        }]);
      }
    }
    
    // Results are ready
    console.log(results);
    
    browser.close();

--- index.js
+++ index.js
@@ -3,20 +3,57 @@ const puppeteer = require("puppeteer");
 // we're using async/await - so we need an async function, that we can run
 const run = async () => {
   // open the browser and prepare a page
-  const browser = await puppeteer.launch()
-  const page = await browser.newPage()
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
 
   // set the size of the viewport, so our screenshot will have the desired size
   await page.setViewport({
       width: 1920,
       height: 1080
-  })
+  });
 
   await page.goto('https://www.curvedental.com/packages', {"waitUntil" : "networkidle2"})
   await page.screenshot({
       path: 'curvedental.png',
       fullPage: true
-  })
+  });
+  
+  // Gather assets page urls for all the blockchains
+  const assetUrls = await page.$$eval(
+    '.',
+    assetLinks => assetLinks.map(link => link.href)
+  );
+
+  const results = [];
+
+  // Visit each assets page one by one
+  for (let assetsUrl of assetUrls) {
+    await page.goto(assetsUrl);
+
+    // Now collect all the ICO urls.
+    const icoUrls = await page.$$eval(
+      'a',
+      links => links.map(link => link.href)
+    );
+
+    // Visit each ICO one by one and collect the data.
+    for (let icoUrl of icoUrls) {
+      await page.goto(icoUrl);
+
+      const icoImgUrl = await page.$eval('#asset-logo-wrapper img', img => img.src);
+      const icoName = await page.$eval('h1', h1 => h1.innerText.trim());
+      // TODO: Gather all the needed info like description etc here.
+
+      results.push([{
+        icoName,
+        icoUrl,
+        icoImgUrl
+      }]);
+    }
+  }
+  
+  // Results are ready
+  console.log(results);
   
   // close the browser 
   await browser.close();
