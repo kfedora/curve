49f28d6acb68da59e74154a26f353fc3d1dcc3b5
k <kfedora@protonmail.com>
2020-10-07 10:49:36 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+const puppeteer = require("puppeteer");
+
+// we're using async/await - so we need an async function, that we can run
+const run = async () => {
+  // open the browser and prepare a page
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+
+  // set the size of the viewport, so our screenshot will have the desired size
+  await page.setViewport({
+      width: 1920,
+      height: 1080
+  })
+
+  await page.goto('https://www.curvedental.com/packages', {"waitUntil" : "networkidle2"})
+  await page.screenshot({
+      path: 'curvedental.png',
+      fullPage: true
+  })
+  
+  // close the browser 
+  await browser.close();
+};
+
+// run the async function
+run();
