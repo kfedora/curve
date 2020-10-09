c9f14c3217a6b8488e8d43e014d8ea62942bf066
k <kfedora@protonmail.com>
2020-10-09 11:40:32 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,61 +2,66 @@ const puppeteer = require("puppeteer");
 
 // we're using async/await - so we need an async function, that we can run
 const run = async () => {
-  // open the browser and prepare a page
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+    // open the browser and prepare a page
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
 
-  // set the size of the viewport, so our screenshot will have the desired size
-  await page.setViewport({
-      width: 1920,
-      height: 1080
-  });
+    // set the size of the viewport, so our screenshot will have the desired size
+    await page.setViewport({
+        width: 1920,
+        height: 1080
+    });
 
-  await page.goto('https://www.curvedental.com/packages', {"waitUntil" : "networkidle2"})
-  await page.screenshot({
-      path: 'curvedental.png',
-      fullPage: true
-  });
-  
-  // Gather assets page urls for all the blockchains
-  const assetUrls = await page.$$eval(
-    '.',
-    assetLinks => assetLinks.map(link => link.href)
-  );
+    const addresses = [
+        "https://www.curvedental.com/dental-charting-software/",
+        "https://www.curvedental.com/dental-scheduling-software/",
+        "https://www.curvedental.com/dental-billing-software",
+        "https://www.curvedental.com/online-dental-forms",
+        "https://www.curvedental.com/patient-portal",
+        "https://www.curvedental.com/dental-report-software",
+        "https://www.curvedental.com/dental-payment-processing",
+        "https://www.curvedental.com/dental-image-software",
+        "https://www.curvedental.com/perio-charting",
+        "https://www.curvedental.com/dental-appointment-reminder-software/",
+        "https://www.curvedental.com/dental-practice-management-software",
+        "https://www.curvedental.com/eprescribe",
+        "https://www.curvedental.com/filesletters",
+        "https://www.curvedental.com/implementation",
+        "https://www.curvedental.com/data-conversion",
+        "https://www.curvedental.com/training",
+        "https://www.curvedental.com/customer-service",
+        "https://www.curvedental.com/dental-image-software#imaging-compatibility",
+        "https://www.curvedental.com/hardware-requirements",
+        "https://www.curvedental.com/status",
+        "https://www.curvedental.com/packages",
+        "https://www.curvedental.com/resources",
+        "https://www.curvedental.com/resources#webinars",
+        "https://www.curvedental.com/blog",
+        "https://www.curvedental.com/curvecares",
+        "https://www.curvedental.com/why-curve-rocks",
+        "https://www.curvedental.com/about",
+        "https://www.curvedental.com/leadership",
+        "https://www.curvedental.com/careers",
+        "https://www.curvedental.com/testimonials",
+        "https://www.curvedental.com/contact",
+        "https://www.curvedental.com/sitemap",
+        "https://www.curvedental.com/why-curve-rocks",
+        "https://go.curvedental.com/demo",
+        "https://www.curvedental.com/partner-list",
+        "https://www.curvedental.com/envoy",
+    ]
 
-  const results = [];
-
-  // Visit each assets page one by one
-  for (let assetsUrl of assetUrls) {
-    await page.goto(assetsUrl);
-
-    // Now collect all the ICO urls.
-    const icoUrls = await page.$$eval(
-      'a',
-      links => links.map(link => link.href)
-    );
-
-    // Visit each ICO one by one and collect the data.
-    for (let icoUrl of icoUrls) {
-      await page.goto(icoUrl);
-
-      const icoImgUrl = await page.$eval('#asset-logo-wrapper img', img => img.src);
-      const icoName = await page.$eval('h1', h1 => h1.innerText.trim());
-      // TODO: Gather all the needed info like description etc here.
-
-      results.push([{
-        icoName,
-        icoUrl,
-        icoImgUrl
-      }]);
+    for (let i = 0; i < addresses.length; i++) {
+        console.log(addresses[i]);
+        await page.goto(addresses[i], { "waitUntil": "networkidle2" })
+        await page.screenshot({
+            path: `curvedental-${i}.png`,
+            fullPage: true
+        });
     }
-  }
-  
-  // Results are ready
-  console.log(results);
-  
-  // close the browser 
-  await browser.close();
+
+    // close the browser 
+    await browser.close();
 };
 
 // run the async function
