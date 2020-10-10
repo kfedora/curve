07c1dcc2d1c446b15f999da4123ff27b6d2c013f
k <kfedora@protonmail.com>
2020-10-10 07:11:09 -0600

    set viewport to 1920x1080

--- curve.js
+++ curve.js
@@ -19,6 +19,10 @@ function crawlPage() {
 
         const browser = await puppeteer.launch(options);
         const page = await browser.newPage();
+        await page.setViewport({
+            width: 1920,
+            height: 1080
+        });
         await page.goto("https://www.curvedental.com/", {
             waitUntil: 'networkidle2',
             timeout: 30000
